b30cb82d449da6b98e9c7b70fbd99adabe421b5f
Jake Lovelace <jaglovelace@gmail.com>
2018-05-17 21:49:30 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -10,11 +10,9 @@ function callMe() {
 
 
 function crazy() {
-  var thisIsCrazy
-  thisIsCrazy= function (){
-                 // fix the code in here:
   thisIsCrazy();
-  console.log("hey!!!")
+  var thisIsCrazy = function (){
+  console.log("hey!!!")  
 
 
 
